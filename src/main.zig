08edd9687837802ef987a5b91013576393a888df
infirit89 <georgedautov808@gmail.com>
2024-08-03 00:37:20 +0300

    started implementing parsing for layer instances

--- src/main.zig
+++ src/main.zig
@@ -6,7 +6,7 @@ const tex2d = rl.Texture2D;
 const vec2 = rl.Vector2;
 const rect = rl.Rectangle;
 
-const GrindTile = struct {
+const GridTile = struct {
     px: Coordinates,
     src: Coordinates,
     f: u32,
@@ -19,17 +19,29 @@ const GrindTile = struct {
     };
 };
 const LayerInstance = struct {
-    identifier: []u8,
-    type: []u8,
-    Width: u32,
-    Height: u32,
-    gridSize: u32,
-    opacity: u32,
-    pxTotalOffsetX: u32,
-    pxTotalOffsetY: u32,
-    tileSetDefUid: u32,
-    tilesetRelativePath: []u8,
-    gridTiles: []GrindTile,
+    identifier: []const u8,
+    type: []const u8,
+    width: i64,
+    height: i64,
+    gridSize: i64,
+    opacity: i64,
+    pxTotalOffsetX: i64,
+    pxTotalOffsetY: i64,
+    tilesetRelativePath: []const u8,
+    gridTiles: std.ArrayList(GridTile),
+    allocator: std.mem.Allocator,
+
+    pub fn init(allocator: std.mem.Allocator) !*LayerInstance {
+        var layerInstance = try allocator.create(LayerInstance);
+        layerInstance.gridTiles = std.ArrayList(GridTile).init(allocator);
+        layerInstance.allocator = allocator;
+        return layerInstance;
+    }
+
+    pub fn deinit(self: *LayerInstance) void {
+        self.gridTiles.deinit();
+        self.allocator.destroy(self);
+    }
 };
 
 const Direction = enum {
@@ -47,11 +59,31 @@ const Level = struct {
     iid: []const u8,
     worldX: i64,
     worldY: i64,
-    Width: i64,
-    Height: i64,
-    layerInstances: ?[]LayerInstance,
-    externalRelativePath: ?[]const u8,
-    neighbours: []Neighbour,
+    width: i64,
+    height: i64,
+    layerInstances: ?std.ArrayList(*LayerInstance),
+    neighbours: std.ArrayList(Neighbour),
+    allocator: std.mem.Allocator,
+
+    pub fn init(allocator: std.mem.Allocator) !*Level {
+        const level = try allocator.create(Level);
+        level.allocator = allocator;
+        level.neighbours = std.ArrayList(Neighbour).init(allocator);
+        return level;
+    }
+
+    pub fn deinit(self: *Level) void {
+        if (self.layerInstances) |layerInstances| {
+            for (layerInstances.items) |layerInstance| {
+                std.debug.print("cum cum cum cum cum cum cum\n", .{});
+                layerInstance.*.deinit();
+            }
+            layerInstances.deinit();
+        }
+
+        self.neighbours.deinit();
+        self.allocator.destroy(self);
+    }
 
     pub fn format(
         self: Level,
@@ -66,19 +98,11 @@ const Level = struct {
         try writer.print("Iid: {s},\n", .{self.iid});
         try writer.print("WorldX: {},\n", .{self.worldX});
         try writer.print("WorldY: {},\n", .{self.worldY});
-        try writer.print("Width: {},\n", .{self.Width});
-        try writer.print("Height: {},\n", .{self.Height});
+        try writer.print("Width: {},\n", .{self.width});
+        try writer.print("Height: {},\n", .{self.height});
 
-        if (self.layerInstances != null) {
-            unreachable;
-        } else {
+        if (self.layerInstances != null) {} else {
             try writer.print("LayerInstances: null,\n", .{});
-        }
-
-        if (self.externalRelativePath != null) {
-            try writer.print("ExternalRelativePath: {s},\n", .{self.externalRelativePath.?});
-        } else {
-            try writer.print("ExternalRelativePath: null,\n", .{});
         }
 
         try writer.print("Neighbours: [],\n", .{});
@@ -86,22 +110,42 @@ const Level = struct {
 };
 
 const World = struct {
-    levels: std.StringHashMap(Level),
+    levels: std.StringHashMap(*Level),
     allocator: std.mem.Allocator,
 
     pub fn init(allocator: std.mem.Allocator) !*World {
         const world = try allocator.create(World);
-        world.levels = std.StringHashMap(Level).init(allocator);
+        world.levels = std.StringHashMap(*Level).init(allocator);
         world.allocator = allocator;
 
         return world;
     }
 
     pub fn deinit(self: *World) void {
+        var it = self.levels.valueIterator();
+        while (it.next()) |value| {
+            value.*.deinit();
+        }
+
         self.levels.deinit();
         self.allocator.destroy(self);
     }
 };
+
+fn readFileAndParseJson(allocator: std.mem.Allocator, path: []const u8) !std.json.Parsed(std.json.Value) {
+    const file = try std.fs.cwd().openFile(
+        path,
+        .{},
+    );
+    defer file.close();
+
+    const file_size = (try file.stat()).size;
+    var buffer = try allocator.alloc(u8, file_size);
+    defer allocator.free(buffer);
+    const bytes_read = try file.readAll(buffer);
+    const parsed = try std.json.parseFromSlice(std.json.Value, allocator, buffer[0..bytes_read], .{});
+    return parsed;
+}
 
 pub fn main() !void {
     rl.initWindow(600, 360, "Test window");
@@ -109,18 +153,8 @@ pub fn main() !void {
     const characterTex: tex2d = rl.loadTexture("./resources/character.png");
     defer rl.unloadTexture(characterTex);
 
-    const file = try std.fs.cwd().openFile(
-        "./resources/maps/overworld_test.ldtk",
-        .{},
-    );
-    defer file.close();
-
-    const file_size = (try file.stat()).size;
-    var buffer = try std.heap.page_allocator.alloc(u8, file_size);
-    defer std.heap.page_allocator.free(buffer);
-    const bytes_read = try file.readAll(buffer);
-    std.debug.print("{s}\n", .{buffer[0..bytes_read]});
-    const parsed = try std.json.parseFromSlice(std.json.Value, std.heap.page_allocator, buffer[0..bytes_read], .{});
+    const mapDirectory = "./resources/maps/";
+    const parsed = try readFileAndParseJson(std.heap.page_allocator, mapDirectory ++ "overworld_test.ldtk");
     defer parsed.deinit();
     const root = parsed.value;
     const levels = root.object.get("levels");
@@ -131,18 +165,44 @@ pub fn main() !void {
         const levelObject = item.object;
         const iid = levelObject.get("iid").?.string;
         // const layerInstance = levelObject.get("layerInstances") orelse null;
-        const externalRelativePath = levelObject.get("externalRelPath") orelse null;
-        const level: Level = .{
-            .identifier = levelObject.get("identifier").?.string,
-            .iid = iid,
-            .worldX = levelObject.get("worldX").?.integer,
-            .worldY = levelObject.get("worldY").?.integer,
-            .Width = levelObject.get("pxWid").?.integer,
-            .Height = levelObject.get("pxHei").?.integer,
-            .layerInstances = null,
-            .externalRelativePath = if (externalRelativePath != null) externalRelativePath.?.string else null,
-            .neighbours = &[0]Neighbour{},
-        };
+        const externalRelativePath = levelObject.get("externalRelPath");
+        var level = try Level.init(std.heap.page_allocator);
+
+        level.identifier = levelObject.get("identifier").?.string;
+        level.iid = iid;
+        level.worldX = levelObject.get("worldX").?.integer;
+        level.worldY = levelObject.get("worldY").?.integer;
+        level.width = levelObject.get("pxWid").?.integer;
+        level.height = levelObject.get("pxHei").?.integer;
+        level.layerInstances = null;
+
+        if (externalRelativePath) |value| {
+            std.debug.print("{s}\n", .{value.string});
+            level.layerInstances = std.ArrayList(*LayerInstance).init(std.heap.page_allocator);
+
+            const paths = [_][]const u8{ mapDirectory, value.string };
+            const levelFilePath = try std.fs.path.join(std.heap.page_allocator, &paths);
+            const parsedLayerInstanceData = try readFileAndParseJson(std.heap.page_allocator, levelFilePath);
+            defer parsedLayerInstanceData.deinit();
+            const layerInstances = parsedLayerInstanceData.value.object.get("layerInstances").?.array;
+
+            for (layerInstances.items) |jsonLayerInstance| {
+                const layerInstanceObject = jsonLayerInstance.object;
+                var layerInstance = try LayerInstance.init(std.heap.page_allocator);
+                layerInstance.identifier = layerInstanceObject.get("__identifier").?.string;
+                layerInstance.type = layerInstanceObject.get("__type").?.string;
+                layerInstance.width = layerInstanceObject.get("__cWid").?.integer;
+                layerInstance.height = layerInstanceObject.get("__cHei").?.integer;
+                layerInstance.gridSize = layerInstanceObject.get("__gridSize").?.integer;
+                layerInstance.opacity = layerInstanceObject.get("__opacity").?.integer;
+                layerInstance.pxTotalOffsetX = layerInstanceObject.get("__pxTotalOffsetX").?.integer;
+                layerInstance.pxTotalOffsetY = layerInstanceObject.get("__pxTotalOffsetY").?.integer;
+                layerInstance.tilesetRelativePath = layerInstanceObject.get("__tilesetRelPath").?.string;
+
+                try level.layerInstances.?.append(layerInstance);
+            }
+        }
+
         try world.levels.put(level.iid, level);
     }
 
