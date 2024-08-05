b99fb54e2c0aa1b7d2d4102cafc4a0fa54899913
infirit89 <georgedautov808@gmail.com>
2024-08-05 22:31:08 +0300

    added basic collision

--- src/main.zig
+++ src/main.zig
@@ -33,13 +33,59 @@ pub fn main() !void {
     defer world.deinit();
 
     std.debug.print("{}\n", .{world});
+    var colliders: std.ArrayList(rect) = undefined;
+    defer colliders.deinit();
 
     const firstLevel = world.levels.get("32bd3de0-4ce0-11ef-9d65-4f44af904a9c");
     var spawnPoint: Point = undefined;
     for (firstLevel.?.layers) |flLayer| {
-        if (flLayer.type == LayerType.Entities) {
-            const playerSpawnPoint = flLayer.entities.get("Player").?;
-            spawnPoint = playerSpawnPoint.worldPos;
+        switch (flLayer.type) {
+            .Entities => {
+                const playerSpawnPoint = flLayer.entities.get("Player").?;
+                spawnPoint = playerSpawnPoint.worldPos;
+            },
+            .IntGrid => {
+                colliders = std.ArrayList(rect).init(std.heap.page_allocator);
+
+                var size: i64 = 0;
+                var x: i64 = 0;
+                var y: i64 = 0;
+                var i: usize = 0;
+                while (i < flLayer.intGrid.len) {
+                    defer i += 1;
+                    const value = flLayer.intGrid[i];
+                    const index: i64 = @intCast(i);
+                    if (value == 1) {
+                        if (size == 0) {
+                            x = @mod(index, flLayer.gridSize);
+                            y = @divFloor(index, flLayer.gridSize);
+                        }
+                        size += 1;
+                        if (@mod(size, flLayer.gridSize) == 0) {
+                            const collider = rect.init(
+                                @floatFromInt(x),
+                                @floatFromInt(y * 32),
+                                @floatFromInt(size * 32),
+                                32.0,
+                            );
+                            try colliders.append(collider);
+                            size = 0;
+                        }
+                    } else if (value == 0) {
+                        if (size > 0) {
+                            const collider = rect.init(
+                                @floatFromInt(x * 32),
+                                @floatFromInt(y * 32),
+                                @floatFromInt(size * 32),
+                                32.0,
+                            );
+                            try colliders.append(collider);
+                            size = 0;
+                        }
+                    }
+                }
+            },
+            else => {},
         }
     }
 
@@ -55,21 +101,28 @@ pub fn main() !void {
         .target = rl.Vector2.init(dest.x + 20, dest.y + 20),
         .rotation = 0.0,
     };
+    var playerCollider: rect = rect.init(
+        dest.x,
+        dest.y * 5,
+        dest.width - 5,
+        dest.height * 0.2,
+    );
     const src: rl.Rectangle = rl.Rectangle.init(1, 6, 15, 22);
     var velocity: vec2 = vec2.zero();
     var rotation: f32 = 0.0;
+    var speed: vec2 = vec2.init(1, 1);
     while (!rl.windowShouldClose()) {
         velocity = vec2.zero();
         if (rl.isKeyDown(.key_a)) {
-            velocity.x -= 1.0;
+            velocity.x -= speed.x;
         } else if (rl.isKeyDown(.key_d)) {
-            velocity.x += 1.0;
+            velocity.x += speed.x;
         }
 
         if (rl.isKeyDown(.key_w)) {
-            velocity.y -= 1.0;
+            velocity.y -= speed.y;
         } else if (rl.isKeyDown(.key_s)) {
-            velocity.y += 1.0;
+            velocity.y += speed.y;
         }
         camera.target = vec2.init(dest.x + 20.0, dest.y + 20.0);
         if (rl.isKeyDown(.key_r)) {
@@ -78,8 +131,13 @@ pub fn main() !void {
         const drag = 0.03;
         velocity = velocity.normalize();
         velocity = velocity.scale(rl.getFrameTime() * 100.0).scale(1 - drag);
+        // const prevX = dest.x;
+        // const prevY = dest.y;
         dest.x += velocity.x;
         dest.y += velocity.y;
+        playerCollider.x = dest.x;
+        playerCollider.y = dest.y + dest.height * 0.8;
+
         rl.beginDrawing();
         defer rl.endDrawing();
         camera.begin();
@@ -124,11 +182,39 @@ pub fn main() !void {
             }
         }
 
+        // rl.drawRectangleRec(playerCollider, rl.Color.dark_green);
+        // for (colliders.items) |collider| {
+        //     rl.drawRectangleRec(collider, rl.Color.green);
+        // }
+        for (colliders.items) |collider| {
+            if (collider.checkCollision(playerCollider)) {
+                const collisionRect = collider.getCollision(playerCollider);
+                if (collisionRect.height >= collisionRect.width) {
+                    if (collisionRect.x <= playerCollider.x + playerCollider.width * 0.5) {
+                        dest.x += collisionRect.width;
+                    } else {
+                        dest.x -= collisionRect.width;
+                    }
+                    speed.x = 0;
+                } else {
+                    if (collisionRect.y <= playerCollider.y + playerCollider.height * 0.5) {
+                        dest.y += collisionRect.height;
+                    } else {
+                        dest.y -= collisionRect.height;
+                    }
+                    speed.y = 0;
+                }
+                // rl.drawRectangleRec(collisionRect, rl.Color.red);
+                break;
+            } else {
+                speed = vec2.init(1, 1);
+            }
+        }
         rl.drawTexturePro(
             characterTex,
             src,
             dest,
-            vec2.init(dest.width / 2.0, dest.height / 2.0),
+            vec2.zero(),
             rotation,
             rl.Color.white,
         );
